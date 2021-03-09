b66bec1f995b82c311a92bf10085f80b92c70801
Megi Cani <canimegi@gmail.com>
2021-03-09 01:07:06 +0100

    first commit

---
+++ makechachematrix.R
@@ -0,0 +1,27 @@
+library(MASS)
+makeCachematrix<- function(x = matrix()){
+  inv<-NULL
+  set<- function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<- function()(x)
+  setInverse<- function(Inverse){Inv<<- Inverse}
+  getInverse<- function(){Inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cachesolve<- function(x,...){
+  Inv<- x$getInverse()
+  if(!is.null(Inv)){
+    message("getting cached data")
+    return(Inv)
+  }
+  mat<- x$get()
+  Inv<- solve(mat,...)
+  x$setInverse(Inv)
+  Inv
+}
+
+
+  
